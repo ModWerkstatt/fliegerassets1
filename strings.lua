b42ac58de9adc3b0ac190cedb26114bd58a1d771
Florian Thiel <flth@flth.de>
2024-11-29 17:46:40 +0100

    initial

---
+++ strings.lua
@@ -0,0 +1,44 @@
+function data()
+	return {
+		de = {
+			["mod_name"] = "Flugzeug Asset Set",
+			["mod_desc"] = "Flugzeugset zum Gestalten von Flugplatzszenen.",
+
+            ["Plane_Assets"] = "Flugzeug Assets",
+            ["Bundeswehr Asset Set"] = "Bundeswehr Asset Set",
+            ["Statische Flugzeuge der Bundeswehr."] = "Statische Flugzeuge der Bundeswehr.",
+            ["Hubschrauber Asset Set"] = "Hubschrauber Asset Set",
+            ["Statische Hubschrauber."] = "Statische Hubschrauber.",
+            ["Luftwaffe Asset Set"] = "Luftwaffe Asset Set",
+            ["Statische Flugzeuge der Luftwaffe."] = "Statische Flugzeuge der Luftwaffe.",
+            ["Segelflug Asset Set"] = "Segelflug Asset Set",
+            ["Statische Segelflugzeuge."] = "Statische Segelflugzeuge.",
+            ["Luftwaffe Asset Set"] = "Luftwaffe Asset Set",
+            ["Statische Flugzeuge/Asset des ersten Weltkrieg."] = "Statische Flugzeuge/Asset des ersten Weltkrieg.",
+            ["als Asset"] = " als Asset",
+            ["Luftwaffe WW1"] = "Luftwaffe WW1",
+            ["Luftwaffe"] = "Luftwaffe",
+            ["Segelflug"] = "Segelflug",
+		},
+		en = {
+			["mod_name"] = "Aircraft Asset Set",
+			["mod_desc"] = "Aircraft collection to set up airfield scenes.",
+
+            ["Plane Assets"] = "Plane Assets",
+            ["Bundeswehr Asset Set"] = "Bundeswehr Asset Set",
+            ["Statische Flugzeuge der Bundeswehr."] = "Static planes of Bundeswehr.",
+            ["Hubschrauber Asset Set"] = "Helicopter Asset Set",
+            ["Statische Hubschrauber."] = "Static helicopter.",
+            ["Luftwaffe Asset Set"] = "Luftwaffe Asset Set",
+            ["Statische Flugzeuge der Luftwaffe."] = "Static planes of Luftwaffe.",
+            ["Segelflug Asset Set"] = "Sailplanes Asset Set",
+            ["Statische Segelflugzeuge."] = "Static sailplanes.",
+            ["Luftwaffe Asset Set"] = "Luftwaffe Asset Set",
+            ["Statische Flugzeuge/Asset des ersten Weltkrieg."] = "Static planes/assets of WW I.",
+            ["als Asset"] = " as asset",
+            ["Luftwaffe WW1"] = "Luftwaffe WWI",
+            ["Luftwaffe"] = "Luftwaffe",
+            ["Segelflug"] = "Sailplane",
+		},
+	}
+end
